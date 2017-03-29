97603974a7babad39220591779b3bea1e7eebd89
Peter Thomas <peterpetrick@gmail.com>
2017-03-29 15:52:32 -0300

    Função Crescente - 3 exemplo

--- cap4.js
+++ cap4.js
@@ -112,17 +112,17 @@ console.log(findSolution(24));
 
 
 // Função Crescente
-function printZeroPaddedWithLabel(number, label) {
-    var numberString = String(number);
-    while (numberString.length < 3)
-        numberString = "0" + numberString;
-    console.log(numberString + " " + label);
+function zeroPad(number, width) {
+    var string = String(number);
+    while (string.length < width)
+        string = "0" + string;
+    return string;
 }
 
 function printFarmInventory(cows, chickens, pigs) {
-    printZeroPaddedWithLabel(cows, "Cows");
-    printZeroPaddedWithLabel(chickens, "Chickens");
-    printZeroPaddedWithLabel(pigs, "Pigs");
+    console.log(zeroPad(cows, 3) + " Cows");
+    console.log(zeroPad(chickens, 3) + " Chickens");
+    console.log(zeroPad(pigs, 3) + " Pigs");
 }
 
 printFarmInventory(7, 11, 3);
