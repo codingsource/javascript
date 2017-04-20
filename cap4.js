50a1c5add109c120d0be359e84cb0e299837980e
Peter Thomas <peterpetrick@gmail.com>
2017-04-20 12:48:12 -0300

    fim conjunto de dados

--- cap4.js
+++ cap4.js
@@ -136,7 +136,7 @@ function g(a, b) {
     return a * b * 3.5;
 }
 
-// Exercicio 
+// Exercicio
 console.log("Exercícios \n Retorna o menor valor");
 
 function min(n1, n2) {
@@ -194,3 +194,59 @@ function countChart(string, char) {
 }
 
 console.log(countChart("kakkerlak", "k"));
+
+console.log("Conjunto de dados!");
+var listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers[1]);
+console.log(listOfNumbers[1 - 1]);
+
+var doh = "Doh";
+console.log(typeof doh.toUpperCase);
+console.log(doh.toUpperCase());
+
+var mack = [];
+mack.push("Mack");
+mack.push("the", "knife");
+console.log(mack);
+console.log(mack.join(" "));
+console.log(mack.pop());
+console.log(mack);
+
+var day1 = {
+    squirrel: false,
+    events: ["work", "touched tree", "pizza", "running", "television"]
+};
+
+console.log(day1.squirrel);
+console.log(day1.wolf);
+day1.wolf = false;
+console.log(day1.wolf);
+
+var descripitions = {
+    work: "Went to work",
+    "touched tree": "Thouched a tree"
+}
+
+var anObject = {
+    left: 1,
+    right: 2
+};
+console.log(anObject.left);
+delete anObject.left;
+console.log(anObject.left);
+console.log("left" in anObject);
+console.log("right" in anObject);
+
+var journal = [{
+        events: ["work", "thouched tree", "pizza", "running", "television"],
+        squirrel: false
+    },
+    {
+        events: ["work", "ice cream", "cauliflower", "lasanga", "touched tree", " brushed teeth"],
+        squirrel: false
+    },
+    {
+        events: ["weekend", "cycling", "break", "peanuts", "beer"],
+        squirrel: true
+    }
+];
