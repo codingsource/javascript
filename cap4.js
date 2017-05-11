59ee8451126363e467322b60ce7e8174f76cc541
Peter Thomas <peterpetrick@gmail.com>
2017-05-11 15:59:14 -0300

    Fução para comparar objetos - Concluindodo Cap 4

--- cap4.js
+++ cap4.js
@@ -565,3 +565,45 @@ function nth(list, position) {
         count++;
     }
 }
+
+console.log("");
+console.log("Deep Comparasion");
+
+function deepEqual(obj1, obj2) {
+    if (obj1 === obj2) return true;
+
+    if (obj1 == null || typeof obj1 != "object" ||
+        obj2 == null || typeof obj2 != "object")
+        return false;
+
+    var c1 = 0,
+        c2 = 0;
+
+    for (var prop in obj1)
+        c1 += 1;
+
+    for (var prop in obj2) {
+        c2 += 1;
+        if (!(prop in obj1) || !deepEqual(obj1[prop], obj2[prop]))
+            return false;
+    }
+    return c1 == c2;
+}
+
+var obj = {
+    here: {
+        is: "an"
+    },
+    object: 2
+};
+console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, {
+    here: 1,
+    object: 2
+}));
+console.log(deepEqual(obj, {
+    here: {
+        is: "an"
+    },
+    object: 2
+}));
